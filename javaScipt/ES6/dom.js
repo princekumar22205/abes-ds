1a6c57eade3ff1c641b78b9a460538566910fece
prince kumar <princekumar22205@gmail.com>
2025-10-14 11:22:18 +0530

    dom

---
+++ javaScipt/ES6/dom.js
@@ -0,0 +1,7 @@
+const div = document.createElement("div");
+const h1 = document.createElement("h1");
+h1.innerText="python is create AI";
+div.appendChild(h1);
+
+const id = document.getElementById("adi");
+id.appendChild(div);
