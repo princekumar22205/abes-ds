1428faf6c64565daee7bc067da3464534e5d03e9
prince kumar <princekumar22205@gmail.com>
2025-10-31 15:36:03 +0530

    card component and nav component

---
+++ frontend/PRINCE/src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Card from './Component/card'
+import Nav from './Component/Nav'
+const App = () => {
+  return (
+    <div>
+      <Nav/>
+      <br/>
+      <center>
+      <Card/>
+      <br/>
+
+      <Card/>
+      <br/>
+
+      <Card/>
+      <br/>
+      
+      </center>
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/PRINCE/src/Component/Nav.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Nav.css'
+const Nav = () => {
+  return (
+    <div id='nav'>
+        <h1>abes</h1>
+    </div>
+  )
+}
+
+export default Nav
